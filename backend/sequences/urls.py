ee10225b75b900807270932d6286bd758789e9f4
fiona.ochs <fiona.oaks@gmail.com>
2021-12-27 16:11:15 -0800

    finding correct CDS

--- backend/sequences/urls.py
+++ backend/sequences/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:sequence_id>', views.ListFileSequences)
+    path('<str:sequence_id>', views.ListFileSequences),
+    path('gb/<str:sequence_id>', views.genBankFile),
+    path('gb2/<str:sequence_id>', views.exampleFile)
 ]
--- backend/sequences/views.py
+++ backend/sequences/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.http import HttpResponse, JsonResponse
 from Bio.Seq import Seq
-from Bio import SeqIO
+from Bio import SeqIO, SeqFeature
 
 
 def ListFileSequences(request, sequence_id):
@@ -35,3 +35,32 @@ def ListFileSequences(request, sequence_id):
 
     data = [{'id': -1, 'sequence': str(sequence_id)}]
     return JsonResponse(data, safe=False)
+
+
+
+def genBankFile(request, sequence_id):
+#     for feature in gb_record.features:
+    foundSeqIdx = 1370
+    sequenceLength = foundSeqIdx + len(sequence_id)
+    for rec in SeqIO.parse("./organisms/NC_000852.5.gb","genbank"):
+        for feature in rec.features:
+             print('location ', feature.location.start.position)
+             print('location end ', feature.location.end.position)
+             startLocation = feature.location.start.position
+             endLocation = feature.location.end.position
+             if feature.type == "CDS" and (startLocation < foundSeqIdx and endLocation > sequenceLength):
+                seq = feature.qualifiers["protein_id"][0]
+                location = feature.location
+                id = ">{} {} {}".format(rec.id, location, rec.description)
+                print("{} \n {}".format(id, *seq))
+                return JsonResponse(str(feature), safe=False)
+#                 return JsonResponse("{} \n {} {}".format(id, *seq, startLocation), safe=False)
+    return JsonResponse(str(gb_record), safe=False)
+
+def exampleFile(request, sequence_id):
+    gbank = SeqIO.parse("./organisms/NC_000852.5.gb", 'genbank')
+    for genome in gbank:
+        print(f'looking in %{genome.id}')
+        for gene in genome.features:
+            if gene.type == 'CDS':
+               return JsonResponse(str(gene), safe=False)
